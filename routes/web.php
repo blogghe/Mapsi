e98e4fffdad216fcfb0fd7b044fad4523af8f17d
bloggh01 <bloggh01@vmuxintrdev03.jetair.be>
2020-02-06 11:08:21 +0100

    e01: Add routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,66 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
+//general pages
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('contact', function (){
+	return 'Contacteer ons';
+});
+
+Route::get('about', function (){
+	return 'Over ons';
+});
+
+//User Pages
+Route::get('user', function (){
+	return 'gebruikers_info';
+});
+
+//Contacts
+Route::get('viewContact', function (){
+	return 'Contact info';
+});
+Route::get('ListContact', function (){
+	return 'Lijst van contacts';
+});
+Route::get('CreateContact', function (){
+	return 'Maak ontact';
+});
+
+//Services
+Route::get('viewService', function (){
+	return 'Service info';
+});
+Route::get('ListService', function (){
+	return 'Lijst van services';
+});
+Route::get('CreateService', function (){
+	return 'Maak service';
+});
+
+//Labels
+Route::get('viewLabel', function (){
+	return 'Contact info';
+});
+Route::get('ListLabel', function (){
+	return 'Lijst van contacts';
+});
+Route::get('CreateLabel', function (){
+	return 'Maak contact';
+});
+
+//Problems
+Route::get('viewProblem', function (){
+	return 'Probleem info';
+});
+Route::get('ListProblem', function (){
+	return 'Lijst van problemen';
+});
+Route::get('CreateProblem', function (){
+	return 'Maak probleem';
 });
