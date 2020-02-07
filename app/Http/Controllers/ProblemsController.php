736171ece2eb81ee0f2d995c3d8b6dc1dd63a339
bloggh01 <bloggh01@vmuxintrdev03.jetair.be>
2020-02-07 15:10:00 +0100

    e12: Make use of 	Compact 	eloquent query 	show lists

--- app/Http/Controllers/ProblemsController.php
+++ app/Http/Controllers/ProblemsController.php
@@ -10,8 +10,24 @@ class ProblemsController extends Controller
 	public function listProblems()
 	{
 		$problems = Problem::all();
+		$reportedProblems = Problem::where( 'status', 0 )->get();
+		$ongoingProblems = Problem::where( 'status', 1 )->get();
+		$pendingProblems = Problem::where( 'status', 2 )->get();
+		$solvedProblems = Problem::where( 'status', 3 )->get();
+		$UnsolvedProblems = Problem::where( 'status', 4 )->get();
 
-		return view( 'problem.list', [ 'problems' => $problems ] );
+		//dd( $UnsolvedProblems );
+
+		/*return view( 'problem.list', [
+			'problems'         => $problems,
+			'reportedProblems' => $reportedProblems,
+			'ongoingProblems' => $ongoingProblems,
+			'pendingProblems' => $pendingProblems,
+			'solvedProblems' => $solvedProblems,
+			'UnsolvedProblems' => $UnsolvedProblems,
+		] );*/
+
+		return view( 'problem.list', compact( 'problems', 'reportedProblems', 'ongoingProblems', 'pendingProblems', 'solvedProblems', 'UnsolvedProblems' ) );
 
 	}
 
@@ -19,13 +35,14 @@ class ProblemsController extends Controller
 	{
 
 		$data = \request()->validate( [
-			'title' => 'required|min:3'
+			'title'  => 'required|min:3',
+			'status' => 'required',
 		] );
 		//dd( \request( 'status' ) );
 		$problem = $this->FillInDefaultProblem();
 		//$description = \request( 'description' );
-		$status = \request('status');
-		$title= \request('title');
+		$status = \request( 'status' );
+		$title = \request( 'title' );
 		//$problem->description = $description;
 		$problem->title = $title;
 
@@ -35,12 +52,14 @@ class ProblemsController extends Controller
 		return back();
 
 	}
+
 	private function FillInDefaultProblem()
 	{
 		$problem = new Problem();
-		$problem->description="";
-		$problem->title="";
-		$problem->status=0;
+		$problem->description = "";
+		$problem->title = "";
+		$problem->status = 0;
+
 		return $problem;
 	}
 
--- resources/views/problem/list.blade.php
+++ resources/views/problem/list.blade.php
@@ -43,17 +43,61 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-12">
-            @if( ! empty($problems))
-                List Problems
+        <div class="col-4">
+            @if( ! empty($reportedProblems))
+                Reported Problems
                 <ul>
-                    @foreach($problems as $problem)
-                        <li>{{$problem->title}}<span class="text-muted"> ({{$problem->status}})</span></li>
-                    <!--<li>{{$problem}}</li>-->
+                    @foreach($reportedProblems as $reportedProblem)
+                        <li>{{$reportedProblem->title}}<span class="text-muted"> ({{$reportedProblem->status}})</span></li>
+                    <!--<li>{{$reportedProblem}}</li>-->
                     @endforeach
                 </ul>
-            @else
-                No problems created yet
+            @endif
+        </div>
+        <div class="col-4">
+            @if( ! empty($ongoingProblems))
+                Ongoing Problems
+                <ul>
+                    @foreach($ongoingProblems as $ongoingProblem)
+                        <li>{{$ongoingProblem->title}}<span class="text-muted"> ({{$ongoingProblem->status}})</span></li>
+                    <!--<li>{{$ongoingProblem}}</li>-->
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+        <div class="col-4">
+            @if( ! empty($pendingProblems))
+                Pending Problems
+                <ul>
+                    @foreach($pendingProblems as $pendingProblem)
+                        <li>{{$pendingProblem->title}}<span class="text-muted"> ({{$pendingProblem->status}})</span></li>
+                    <!--<li>{{$pendingProblem}}</li>-->
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            @if( ! empty($solvedProblems))
+                Solved Problems
+                <ul>
+                    @foreach($solvedProblems as $solvedProblem)
+                        <li>{{$solvedProblem->title}}<span class="text-muted"> ({{$solvedProblem->status}})</span></li>
+                    <!--<li>{{$solvedProblem}}</li>-->
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+        <div class="col-6">
+            @if( ! empty($UnsolvedProblems))
+                Unsolved Problems
+                <ul>
+                    @foreach($UnsolvedProblems as $UnsolvedProblem)
+                        <li>{{$UnsolvedProblem->title}}<span class="text-muted"> ({{$UnsolvedProblem->status}})</span></li>
+                    <!--<li>{{$UnsolvedProblem}}</li>-->
+                    @endforeach
+                </ul>
             @endif
         </div>
     </div>
